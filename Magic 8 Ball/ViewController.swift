9152243eb59c0039983239a600d8409378f80a09
kesumanjati <kesumanjati@gmail.com>
2019-05-22 13:29:12 +0700

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Galuh Kesumanjati on 22/05/19.
+//  Copyright © 2019 ADa team 07. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
